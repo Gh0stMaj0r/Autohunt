1029d5199806c0568b31ce4787c3b3f9bd1afaa2
Gh0stMaj0r <suhonen.arttu2002@gmail.com>
2024-03-15 13:13:44 +0200

    Update

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -11,9 +11,16 @@ const Navbar = () => {
 				<div className="header-links">
 					<a className='link' href='#landing'>Home</a>
 					<a className='link' href='#palvelut'>Models</a>
-					<a className='link' href='#kuvagalleria'>Parts</a>
+					<a className='link' href='#palvelut'>Sell your car</a>
 					<a className='link' href='#yhteystiedot'>News</a>
                     <a className='link' href='#yhteystiedot'>Contact</a>
+
+					<div className="switch">
+        				<label className="theme-switch">
+            				<input type="checkbox" id="checkbox" />
+            				<div className="slider round"></div>
+        				</label>
+    				</div>
 				</div>
 			</div>
 		</header>
