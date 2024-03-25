2e4763d0ece40957654a3d04ec8649d0d2037675
Gh0stMaj0r <suhonen.arttu2002@gmail.com>
2024-03-25 14:07:13 +0200

    Footer Update
    - First version of footer done

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -2,11 +2,59 @@ import React from "react";
 
 import './Footer.scss'
 
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
     return (
         <section id="footer" className="wrapper">
             <div className="content">
+
+                <div className="footer-flex">
                 
+                    <div className="footer-column">
+                        <img src="./Images/New-Logo.png" alt="Logo" className="footer-logo" />
+                        <p>"Unleash the Speed and Experience Thrill"<br></br> - with Autohunt</p>
+                    </div>
+
+                    <div className="footer-column">
+                        <h3>Useful Links</h3>
+                        <a>About Us</a>
+                        <a>FAQ</a>
+                        <a>Contact</a>
+                    </div>
+
+                    <div className="footer-column">
+                        <h3>Customers Service</h3>
+                        <a>support@autohunt.com</a>
+                        <a>240-865-3730</a>
+                    </div>
+
+                    <div className="footer-column">
+                        <h3>Location</h3>
+                        <a>Leland Avenue, Maryland</a>
+                        <a>21220, United States</a>
+                    </div>
+
+                    <div className="footer-column">
+                        <h3>Our Social Media</h3>
+                        <div className="social">
+                            <a><FaFacebook/></a>
+                            <a><FaInstagram/></a>
+                            <a><FaYoutube/></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="copyright-legal">
+                    <p>Copyright © 2024 Autohunt Ltd.<br></br> All Rights Reserved</p>
+                    
+                    <div className="legal-info">
+                        <a>Terms Of Service</a>
+                        <a>Privacy Policy</a>
+                        <a>EULA</a>
+                    </div>
+                </div>
+
             </div>
         </section>
     )
--- src/Pages/Home/elements/Contact/index.tsx
+++ src/Pages/Home/elements/Contact/index.tsx
@@ -10,6 +10,7 @@ const Contact = () => {
                 <div className="contact-row">
                 <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3085.988892983447!2d-76.43936704313612!3d39.33383716898806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c7fd46c7cb8043%3A0xf0226bac876ec7fd!2sLeland%20Ave%2C%20Middle%20River%2C%20MD%2021220%2C%20Yhdysvallat!5e0!3m2!1sfi!2sfi!4v1711105444209!5m2!1sfi!2sfi" 
+                title="Maps Location"
                 width="600" 
                 height="450" 
                 loading="lazy"
